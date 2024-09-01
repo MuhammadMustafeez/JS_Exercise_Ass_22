bfc36dae38d3ccf15d19e6505c5dd89d02252b57
MuhammadMustafeez <mustafeez.azmat123@gmail.com>
2024-09-01 10:16:17 +0500

    Assignment

---
+++ index.js
@@ -0,0 +1,72 @@
+var questions= [
+    {
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+    {
+    Q1 : "What is CSS stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText",
+    Correct : Option1
+},
+    {
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+    {
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+{
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+{
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+{
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+{
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+{
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+{
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
+    Option2 : "Hyper Text Transport Language",
+    Option3 : "HyperText ",
+    Correct : Option1
+},
+]
