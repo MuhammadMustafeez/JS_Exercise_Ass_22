ba94befae3b195d786ee71f2493c1ed7814267f5
MuhammadMustafeez <mustafeez.azmat123@gmail.com>
2024-09-01 10:25:47 +0500

    Javascript File Updated

--- index.js
+++ index.js
@@ -9,64 +9,15 @@ var questions= [
     {
     Q1 : "What is CSS stand for",
     Option1 : "HyperText Markup language",
+    Option2 : "Cascading Style Sheet",
+    Option3 : "coloring",
+    Correct : Option1
+},
+    {
+    Q1 : "What is HTML stand for",
+    Option1 : "HyperText Markup language",
     Option2 : "Hyper Text Transport Language",
     Option3 : "HyperText",
     Correct : Option1
-},
-    {
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-    {
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-{
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-{
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-{
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-{
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-{
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
-{
-    Q1 : "What is HTML stand for",
-    Option1 : "HyperText Markup language",
-    Option2 : "Hyper Text Transport Language",
-    Option3 : "HyperText ",
-    Correct : Option1
-},
+}
 ]
